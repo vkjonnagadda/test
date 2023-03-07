4892c2fdfe8a950e4fd4a9fa4cc55e5f20c209d9
vkjonnagadda <vkjonnagadda@gmail.com>
2023-03-07 10:33:33 +0000

    initial commits by VK

---
+++ src/main/java/Demo/Demo_Project/App.java
@@ -0,0 +1,13 @@
+package Demo.Demo_Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
